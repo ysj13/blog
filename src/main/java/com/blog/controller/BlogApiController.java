686b5c06d7b5438403d95abd2d1d0b4965dd7809
joon <ysj13@kakao.com>
2023-08-29 01:15:09 +0900

    글 전체조회 로직

--- src/main/java/com/blog/controller/BlogApiController.java
+++ src/main/java/com/blog/controller/BlogApiController.java
@@ -2,13 +2,17 @@ package com.blog.controller;
 
 import com.blog.domain.Article;
 import com.blog.dto.AddArticleRequest;
+import com.blog.dto.ArticleResponse;
 import com.blog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,5 +25,16 @@ public class BlogApiController {
         Article saveArticle = blogService.save(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(saveArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
--- src/main/java/com/blog/service/BlogService.java
+++ src/main/java/com/blog/service/BlogService.java
@@ -6,6 +6,8 @@ import com.blog.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class BlogService {
@@ -15,5 +17,9 @@ public class BlogService {
     // 블로그 글 저장
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
