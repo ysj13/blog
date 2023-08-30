3a72de84f9ccdf8f8b38d913ce9a6a11c25db121
joon <ysj13@kakao.com>
2023-08-30 12:39:54 +0900

    블로그 게시글 조회, 삭제 API 구현

--- src/main/java/com/blog/controller/BlogApiController.java
+++ src/main/java/com/blog/controller/BlogApiController.java
@@ -7,10 +7,7 @@ import com.blog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,5 +33,21 @@ public class BlogApiController {
 
         return ResponseEntity.ok()
                 .body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable Long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
+    }
+
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable Long id) {
+        blogService.delete(id);
+
+        return ResponseEntity.ok()
+                .build();
     }
 }
--- src/main/java/com/blog/service/BlogService.java
+++ src/main/java/com/blog/service/BlogService.java
@@ -22,4 +22,17 @@ public class BlogService {
     public List<Article> findAll() {
         return blogRepository.findAll();
     }
+
+    public Article findById(Long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException(id + " 번 째 글을 찾을 수 없습니다."));
+    }
+
+    public void delete(Long id) {
+        blogRepository.deleteById(id);
+    }
+
+    public void update(Long id) {
+
+    }
 }
