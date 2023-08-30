ac1f7d41d8a120aed667aae4f4d717c7a719f1b1
joon <ysj13@kakao.com>
2023-08-30 17:43:39 +0900

    인증을 위한 회원 도메인, 리포지터리, 서비스 추가

---
+++ src/main/java/com/blog/domain/User.java
@@ -0,0 +1,77 @@
+package com.blog.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class User implements UserDetails {  // UserDetails : 스프링 시큐리티에서 사용자의 인증 정보를 담아두는 인터페이스
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Builder
+    public User(String email, String password, String auth) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override   // 권한 반환
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override   // 사용자의 id를 반환(고유한 값)
+    public String getUsername() {
+        return email;
+    }
+
+    @Override   // 사용자의 패스워드 반환
+    public String getPassword() {
+        return password;
+    }
+
+    @Override   // 계정 만료 여부 반환
+    public boolean isAccountNonExpired() {
+        // 만료되었는지 확인하는 로직 추가
+        // true : 만료되지 않았음
+        return true;
+    }
+
+    @Override   // 계정 잠금 여부 반환
+    public boolean isAccountNonLocked() {
+        // 계정이 잠겼는지 확인하는 로직 추가
+        // true : 잠금되지 않았음
+        return false;
+    }
+
+    @Override   // 패스워드의 만료 여부 반환
+    public boolean isCredentialsNonExpired() {
+        // 패스워드가 만료되었는지 확인하는 로직
+        // true : 만료되지 않았음
+        return false;
+    }
+
+    @Override   // 계정 사용 가능 여부 반환
+    public boolean isEnabled() {
+        // 계정이 사용 가능한지 확인하는 로직
+        // true : 사용 가능
+        return false;
+    }
+}
---
+++ src/main/java/com/blog/service/UserDetailService.java
@@ -0,0 +1,21 @@
+package com.blog.service;
+
+import com.blog.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {  // 스프링 시큐리티에서 사용자 정보를 가져오는 인터페이스
+
+    private final UserRepository userRepository;
+
+    // 사용자 이름(email)으로 사용자의 정보를 가져오는 메소드
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException(email));
+    }
+}
