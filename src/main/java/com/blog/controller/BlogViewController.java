f6aa7be51ca87ebc0fd726b292cf3ab326be10c3
joon <ysj13@kakao.com>
2023-08-30 17:13:15 +0900

    글 등록, 수정기능 추가

--- src/main/java/com/blog/controller/BlogViewController.java
+++ src/main/java/com/blog/controller/BlogViewController.java
@@ -2,12 +2,14 @@ package com.blog.controller;
 
 import com.blog.domain.Article;
 import com.blog.dto.ArticleListViewResponse;
+import com.blog.dto.ArticleViewResponse;
 import com.blog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -35,4 +37,17 @@ public class BlogViewController {
 
         return "article";
     }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
+    }
+
 }
