546d5be5f4e66565d5bdea248c9b79c5290974e5
joon <ysj13@kakao.com>
2023-09-02 03:36:38 +0900

    리프레시 토큰 구현

---
+++ src/main/java/com/blog/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.blog.repository;
+
+import com.blog.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
